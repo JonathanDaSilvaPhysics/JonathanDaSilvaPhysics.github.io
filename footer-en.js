aa8bb51ab0521015e492aea1c68c87534da5f2ba
Jonathan Da Silva <jonathan.da.silva.physics@gmail.com>
2023-07-04 23:58:08 +0200

    feat: new pictures

--- footer-en.js
+++ footer-en.js
@@ -1,7 +1,7 @@
 // Display contact section :
 
 document.write("<div class='contact'><h2>Contact</h2><p>Jonathan Da Silva <br />31 770 Colomiers, France <br />Email : <a href='mailto:jonathan.da.silva.physics@gmail.com'>jonathan.da.silva.physics@gmail.com</a></p>");
-document.write("<p> <span class='end'>Last update : 21 june 2023</span></p>");
+document.write("<p> <span class='end'>Last update : 4 july 2023</span></p>");
 document.write("<p><a style='border-bottom:none' href='http://validator.w3.org/check?uri=jonathandasilvaphysics.github.io'><img style='border:0;width:39px;height:34px' title='HTML5 validation' src='images/html5-logo.png' alt='HTML5 validation'></a>");
 document.write("<a style='border-bottom:none' href='http://jigsaw.w3.org/css-validator/validator?uri=jonathandasilvaphysics.github.io'><img style='border:0;width:37px;height:34px' title='CSS3 validation' src='images/css3-logo.png' alt='CSS3 validation'></a> </p></div>");
 
@@ -1043,11 +1043,17 @@ text[1032]="Landscape near Saissac with the Lauragais plain and the Pyrenees in 
 text[1033]="Scenery of the area of Saissac near its castle";
 text[1034]="Landscape around Saissac castle with the Lauragais plain and the Pyrenees in background";
 text[1035]="View over the Castrais plain from the road of the Croix de Montalric";
+text[1036]="Goulier-Neige ski resort in summer";
+text[1037]="Scenery seen from Goulier-Neige ski resort towards Suc and Sentenac (center)";
+text[1038]="Greenery around Goulier-Neige ski resort with Goulier's church, Suc and Sentenac in background";
+text[1039]="View over Goulier-Neige ski resort in front of the Pique d'Endron";
+text[1040]="Goulier and its surroundings with the Pique d'Endron in background";
+text[1041]="Landscape seen near Goulier with Olbier, ruins of Montr&eacute;al-de-Sos castle and Saleix (in background)";
 
 // define the approximate values "latitude,longitude" of the associated picture for google maps (in decimal degrees)
 // array "latitudeLongitude" defined in Coordinates.js
 
-var i = Math.floor(1035*Math.random()+1);  // random number between 1 and 1035
+var i = Math.floor(1041*Math.random()+1);  // random number between 1 and 1041
 
 // now display the picture + the text + the google maps link in a banner:
 if (isLarge[i]) {
--- footer-fr.js
+++ footer-fr.js
@@ -1,7 +1,7 @@
 // Afficher la partie contact :
 
 document.write("<div class='contact'><h2>Contact</h2><p>Jonathan Da Silva <br />31 770 Colomiers, France <br />Email : <a href='mailto:jonathan.da.silva.physics@gmail.com'>jonathan.da.silva.physics@gmail.com</a></p>");
-document.write("<p> <span class='end'>Derni&egrave;re mise &agrave; jour : 21 juin 2023</span></p>");
+document.write("<p> <span class='end'>Derni&egrave;re mise &agrave; jour : 4 juillet 2023</span></p>");
 document.write("<p><a style='border-bottom:none' href='http://validator.w3.org/check?uri=jonathandasilvaphysics.github.io'><img style='border:0;width:39px;height:34px' title='Validation HTML5' src='images/html5-logo.png' alt='Validation HTML5'></a>");
 document.write("<a style='border-bottom:none' href='http://jigsaw.w3.org/css-validator/validator?uri=jonathandasilvaphysics.github.io'><img style='border:0;width:37px;height:34px' title='Validation CSS3' src='images/css3-logo.png' alt='Validation CSS3'></a> </p></div>");
 
@@ -1043,11 +1043,17 @@ text[1032]="Paysage pr&egrave;s de Saissac avec la plaine du Lauragais et les Py
 text[1033]="D&eacute;cor de la r&eacute;gion de Saissac pr&egrave;s de son ch&acirc;teau";
 text[1034]="Paysage autour du ch&acirc;teau de Saissac avec la plaine du Lauragais et les Pyr&eacute;n&eacute;es en arri&egrave;re-plan";
 text[1035]="Vue sur la plaine du Castrais &agrave; partir de la route de la croix de Montalric";
+text[1036]="La station de ski de Goulier-Neige, en &eacute;t&eacute;";
+text[1037]="D&eacute;cor vu de la station de ski de Goulier-Neige, en direction de Suc et Sentenac (centre)";
+text[1038]="Verdure autour de la station de ski de Goulier-Neige avec l'&eacute;glise de Goulier, Suc et Sentenac en arri&egrave;re-plan";
+text[1039]="Vue sur la station de ski de Goulier-Neige devant la Pique d'Endron";
+text[1040]="Goulier et ses environs avec la Pique d'Endron en arri&egrave;re-plan";
+text[1041]="Paysage pr&egrave;s de Goulier avec Olbier, les ruines du ch&acirc;teau de Montr&eacute;al-de-Sos et Saleix (en arri&egrave;re-plan)";
 
 // definir les valeurs approximatives "latitude,longitude" de l'image associee pour google maps (en degres decimaux)
 // tableau "latitudeLongitude" definie dans Coordinates.js
 
-var i = Math.floor(1035*Math.random()+1);  // nombre aleatoire entre 1 et 1035
+var i = Math.floor(1041*Math.random()+1);  // nombre aleatoire entre 1 et 1041
 
 // maintenant, afficher l'image + le texte + le lien vers google maps dans une banniere :
 if (isLarge[i]) {
