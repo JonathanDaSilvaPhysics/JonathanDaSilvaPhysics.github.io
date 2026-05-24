418daf03ae7e3455072a44584b378fee78b610e1
Jonathan Da Silva <jonathan.da.silva.physics@gmail.com>
2026-05-24 21:20:03 +0200

    feat: new pictures
    add an intellij script to launch the website

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-const lastUpdate = new Date("2026-05-14");
+const lastUpdate = new Date("2026-05-24");
 const lang = ["fr", "en"].includes(document.documentElement.lang)
     ? document.documentElement.lang
     : "en";
