6ffb75f6090b925451770e68070c972f6bdeee8d
Jonathan Da Silva <jonathan.da.silva.physics@gmail.com>
2026-09-13 22:12:55 +0200

    feat: new pictures

--- footer.js
+++ footer.js
@@ -1,4 +1,4 @@
-const lastUpdate = new Date("2026-09-11");
+const lastUpdate = new Date("2026-09-13");
 const lang = ["fr", "en"].includes(document.documentElement.lang)
     ? document.documentElement.lang
     : "en";
